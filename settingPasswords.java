7ce38221c75de95506170b29e290ff70540a0687
megs <rogerm21@mcmaster.ca>
2024-11-10 12:19:32 -0500

    fixed some test cases in settingPasswords.java

--- settingPasswords.java
+++ settingPasswords.java
@@ -1,26 +1,35 @@
 import java.util.*;
 public class Password {
 
-    // username, website, password
     private static Map<String, Map<String, String>> usernameMap = new HashMap<>();
 
-    // method that returns the map for the coressponding user
+    // method that returns the map for the corresponding user
     private static Map getUsernameMap(String username) {
         Map userMap = usernameMap.get(username);
         return userMap;
     }
 
-    public static void setUserToPassword(String username, String website, String password){
+    public static boolean setUserToPassword(String username, String website, String password){
+
         // if the username already exits, get the map
         if (usernameMap.containsKey(username)){
             Map userPassword = getUsernameMap(username); // get map
-            userPassword.put(website, password); // generate new key value pair
+
+            // check if website already has a password
+            if (userPassword.containsKey(website)){
+                System.out.println("password for this website already exists!");
+                return false;
+            }
+            else{ // generate new key value pair
+                userPassword.put(website, password);
+            }
         }
         else{ // if the username does not exist, create a new map
             Map<String, String> userPassword = new HashMap<>(); // make new map
             userPassword.put(website, password); // store website password pair in new map
             usernameMap.put(username, userPassword); // set the userPassword to the corresponding username
         }
+        return true;
     }
 
     // fetch the website's corresponding password
@@ -31,8 +40,12 @@ public class Password {
 
     // fetch the username's password
     public static String getUsernamePassword(String username, String website){
-        Map userMap = getUsernameMap(username);
-        String password = getWebsitePassword(website, userMap);
+
+        if (usernameMap.containsKey(username) == false){
+            return null;
+        }
+        Map userMap = getUsernameMap(username); // get the map for the usernames
+        String password = getWebsitePassword(website, userMap); // get the
         return password;
     }
 
@@ -46,5 +59,12 @@ public class Password {
 
         String password2 = getUsernamePassword("numberTwo", "websiteThree");
         System.out.println(password2);
+
+        String password3 = getUsernamePassword("numberTwo", "websiteFour"); // if website doesn't exist, return null
+        System.out.println(password3);
+
+        // if username doesn't exist return null
+        String password4 = getUsernamePassword("numberThree", "websiteOne");
+        System.out.println(password4);
     }
 }
